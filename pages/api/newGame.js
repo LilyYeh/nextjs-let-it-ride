f10905ee254cc76a13a7811e8651fc0bb925498b
LilyYeh <lilyyeh@core-tech.tw>
2022-11-09 11:39:26 +0800

    adjust some code

--- pages/api/newGame.js
+++ pages/api/newGame.js
@@ -1,11 +1,20 @@
-import { setNewGame } from "../../lib/db_players";
+import { getPlayers, setNewGame } from "../../lib/db_players";
+import { createBanker, getBanker } from "../../lib/db_banker";
 
 export default async function handler(req, res) {
 	try {
 		const baseMoney = JSON.parse(req.body).baseMoney;
 		const baseMyMoney = JSON.parse(req.body).baseMyMoney;
-		const totalMoney = JSON.parse(req.body).totalMoney;
-		const players = await setNewGame(baseMoney, baseMyMoney, totalMoney);
+
+		// 建立莊家
+		await createBanker();
+		const banker = await getBanker();
+		let bankerId = banker[0].id;
+
+		const data = {bankerId:bankerId, money: baseMyMoney-baseMoney};
+		await setNewGame(data)
+
+		const players = await getPlayers();
 		res.status(200).json(players);
 	}catch (error) {
 		res.status(500).json({ error:error.message });
--- pages/api/playerLogin.js
+++ pages/api/playerLogin.js
@@ -1,11 +1,42 @@
-import { login } from "../../lib/db_players";
+import { countPlayers, createPlayer, getPlayers } from "../../lib/db_players";
+import { createBanker, getBanker } from "../../lib/db_banker";
 
 export default async function handler(req, res) {
 	try {
 		const socketId = JSON.parse(req.body).socketId;
 		const baseMoney = JSON.parse(req.body).baseMoney;
 		const baseMyMoney = JSON.parse(req.body).baseMyMoney;
-		const players = await login(socketId, baseMoney, baseMyMoney);
+		let totalPlayers = await countPlayers();
+
+		let isCurrentPlayer = 0;
+		if(totalPlayers === 0) {
+			isCurrentPlayer = 1;
+			// 建立莊家
+			await createBanker();
+		}
+
+		const banker = await getBanker();
+		//ranking 狀態
+		let bankerId = 0;
+		if(banker.length !== 0){
+			bankerId = banker[0].id;
+		}
+
+		const playerData = {
+			socketId: socketId,
+			isCurrentPlayer: isCurrentPlayer,
+			money: baseMyMoney-baseMoney,
+			bankerId: bankerId
+		}
+
+		await createPlayer(playerData);
+
+		if(banker.length == 0){
+			res.status(200).json(false);
+			return false;
+		}
+
+		const players = await getPlayers();
 		res.status(200).json(players);
 	}catch (error) {
 		res.status(500).json({ error:error.message });
